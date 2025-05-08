912fc4f34445c96f4648b00dc6bf71117cb366dc
Okko Heinio <okko.heinio@gmail.com>
2025-05-08 16:20:33 +0300

    differentiation s17s1

--- FormalisingMathematics2024/Section17curvesAndSurfaces/Sheet1.lean
+++ FormalisingMathematics2024/Section17curvesAndSurfaces/Sheet1.lean
@@ -7,6 +7,9 @@ import Mathlib.Tactic
 import Mathlib.Analysis.Calculus.ParametricIntegral
 import Mathlib.Analysis.Calculus.ContDiff.Defs
 
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Deriv
+import Mathlib.Analysis.Calculus.FDeriv.Comp
+
 
 /-
 
@@ -66,16 +69,77 @@ open Real
 -- This says "the cos(sin(x))*exp(x) is differentiable"
 -- Hint: the theorems are called theorems like `differentiable.mul` etc.
 -- Try proving it by hand.
-example : Differentiable ℝ fun x => cos (sin x) * exp x := by sorry
+example : Differentiable ℝ fun x => cos (sin x) * exp x := by
+  apply Differentiable.mul
+  · change Differentiable _ (cos ∘ sin)
+    apply Differentiable.comp
+    · exact differentiable_cos
+    · exact differentiable_sin
+  · exact differentiable_exp
 
 -- Now see what `hint` has to say!
-example : Differentiable ℝ fun x => cos (sin x) * exp x := by sorry
+example : Differentiable ℝ fun x => cos (sin x) * exp x := by
+  -- wishlist: shortcut to generate · sorry for each unsolved goal. stop automatic formatting from removing a space before ·
+  intro x
+  apply DifferentiableAt.mul
+  · change DifferentiableAt _ (cos ∘ sin) _
+    apply DifferentiableAt.comp
+    · exact differentiableAt_cos
+    · exact differentiableAt_sin
+  ·
+    exact differentiableAt_exp
+
+
 
 -- The simplifier used to be able to do this
 example (x : ℝ) :
-    deriv (fun x => cos (sin x) * exp x) x = (cos (sin x) - sin (sin x) * cos x) * exp x :=
-  sorry
+    deriv (fun x => cos (sin x) * exp x) x = (cos (sin x) - sin (sin x) * cos x) * exp x := by
+  set der := (cos (sin x) - sin (sin x) * cos x)
+  rw [deriv_mul _ _]
+  rotate_left
+  · change DifferentiableAt _ (cos ∘ sin) _
+    apply DifferentiableAt.comp
+    · exact differentiableAt_cos
+    · exact differentiableAt_sin
+  · exact differentiableAt_exp
+
+  -- have : DifferentiableAt ℝ ((fun x ↦ cos (sin x))) x := by
+  rw [Real.deriv_exp]
+  suffices deriv (fun x ↦ cos (sin x)) x  + cos (sin x) = der by
+    rw [←this]
+    ring
+  suffices deriv (fun x ↦ cos (sin x)) x = - sin (sin x) * cos x by
+    rw [this]
+    ring
+
+  have cos_deriv (y): deriv cos y = - sin y := by exact Real.deriv_cos
+  have sin_deriv (x): deriv sin x = cos x := by rw [Real.deriv_sin]
+  simp only [neg_mul]
+  -- rw [←sin_deriv,←cos_deriv]
+  rw [deriv]
+  erw [fderiv.comp (f := sin) (g:= cos) x _ _]
+  rotate_left
+  · exact differentiableAt_cos
+  · exact differentiableAt_sin
+  simp only [ContinuousLinearMap.coe_comp', Function.comp_apply]
+  rw [fderiv_sin _]
+  rotate_left
+  · exact differentiableAt_id'
+  simp only [fderiv_id', ContinuousLinearMap.coe_smul', ContinuousLinearMap.coe_id', Pi.smul_apply,
+    id_eq, smul_eq_mul, mul_one]
+  rw [fderiv_cos]
+  rotate_left
+  · exact differentiableAt_id'
+  simp only [fderiv_id', neg_smul, ContinuousLinearMap.neg_apply, ContinuousLinearMap.coe_smul',
+    ContinuousLinearMap.coe_id', Pi.smul_apply, id_eq, smul_eq_mul]
+
 
 --by { simp, ring }
 -- Try this one:
-example (a : ℝ) (x : ℝ) : DifferentiableAt ℝ (fun y : ℝ => exp (-(a * y ^ 2))) x := by sorry
+example (a : ℝ) (x : ℝ) : DifferentiableAt ℝ (fun y : ℝ => exp (-(a * y ^ 2))) x := by
+  apply DifferentiableAt.comp (g:= rexp)
+  ·
+    exact differentiableAt_exp
+  ·
+    simp only [differentiableAt_neg_iff, differentiableAt_const, differentiableAt_id',
+      DifferentiableAt.pow, DifferentiableAt.mul]
