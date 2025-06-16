3b6d58ce103e6376c82c0f40c30bca7bdace95e3
okkokko10 <okko.heinio@gmail.com>
2025-06-16 08:04:29 +0300

    same_result_equiv

--- FormalisingMathematics2024/Okko/computation.lean
+++ FormalisingMathematics2024/Okko/computation.lean
@@ -542,6 +542,11 @@ def StateAutomaton.same_accept {O' : Type} (A : StateAutomaton I O) (B : StateAu
 -- def StateAutomaton.same_result (A B : StateAutomaton I O) (h : same_accept A B) : Prop := ∀ t : I, result A t = (fun (c : accepts A t) ↦ result B t ((h t).mp c))
 -- the acceptance and results are the same
 def StateAutomaton.same_result (A B : StateAutomaton I O): Prop := ∃(h : same_accept A B), ∀ t : I, ∀(c : accepts A t), result A t c = result B t ((h t).mp c)
+theorem StateAutomaton.same_result_def (A B : StateAutomaton I O) : same_result A B ↔
+    same_accept A B ∧ ∀ (t : I) (ac : accepts A t) (bc : accepts B t), result A t ac = result B t bc := by
+  unfold same_result
+  aesop
+
 
 
 def StateAutomaton.conversion : O := get M <| init M tape
@@ -588,6 +593,49 @@ def StateAutomaton.comp {X : Type} (A : StateAutomaton I X) (B : StateAutomaton 
 def StateAutomaton.models_function_accept (f : I → Prop) : Prop := ∀t : I, accepts M t ↔ (f t)
 def StateAutomaton.models_function (f : I → Option O) : Prop :=
     (models_function_accept M (f · |>.isSome)) ∧  ∀t : I, ∀(c : accepts M t), match (f t) with | some w => result M t c = w | none => False
+
+-- #check Equivalence
+-- #check RelEmbedding
+-- #check Function.Embedding
+instance StateAutomaton.same_accept_equiv : @Equivalence (StateAutomaton I O) same_accept where
+  refl M := by
+    unfold same_accept
+    intro t
+    rfl
+  symm := by
+    unfold same_accept
+    -- aesop
+    intro A B h t
+    symm
+    apply h
+  trans := by
+    unfold same_accept
+    intro x y z xy yz t
+    rw [xy,yz]
+
+-- theorem StateAutomaton.same_accept_equiv' (A B : StateAutomaton I O) : same_accept A B ↔ StateAutomaton.same_accept_equiv A B := by rfl
+
+def StateAutomaton.same_result_equiv : @Equivalence (StateAutomaton I O) same_result where
+  refl M := by
+    unfold same_result
+    simp only [implies_true, exists_prop, and_true]
+    apply same_accept_equiv.refl
+  symm := by
+    simp_rw [same_result_def]
+    intro x y ⟨xy_a,xy_eq⟩
+    refine ⟨same_accept_equiv.symm xy_a,?_⟩
+    intro t yc xc
+    exact xy_eq t xc yc |>.symm
+  trans := by
+    simp_rw [same_result_def]
+    intro x y z ⟨xy_a,xy_eq⟩ ⟨yz_a,yz_eq⟩
+    refine ⟨same_accept_equiv.trans xy_a yz_a,?_⟩
+    intro t xc zc
+    have yc:= (xy_a t).mp xc
+    rw [xy_eq t xc yc]
+    exact yz_eq t yc zc
+
+-- todo: a relation for models_function
 
 
 end stateAutomaton
